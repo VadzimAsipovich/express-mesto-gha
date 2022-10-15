3e32da02bb6c999adf0918c66e16c1d5cdb5d50d
vadzim.asipovich <vadimosipovichqa@yandex.by>
2022-10-15 17:58:31 +0300

    fix 404 route

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 app.use("/users", require("./routes/users"));
 app.use("/cards", require("./routes/cards"));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.status(404).send({ message: "Запрошенной страницы не существует." });
 });
 
