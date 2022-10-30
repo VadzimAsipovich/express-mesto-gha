d9dce0010c0990408dca0659a021eb818d2bb88a
vadzim.asipovich <vadimosipovichqa@yandex.by>
2022-10-31 00:01:19 +0300

    fff

--- routes/cards.js
+++ routes/cards.js
@@ -8,13 +8,15 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
+const urlPattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+
 router.get('/', getCards);
 router.post(
   '/',
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
-      link: Joi.string().required(),
+      link: Joi.string().required().regex(urlPattern),
     }),
   }),
   createCard,
