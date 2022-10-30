0b32eaf5f8dcea321d478bf7d1e9f9b3c97b73d8
vadzim.asipovich <vadimosipovichqa@yandex.by>
2022-10-30 20:54:36 +0300

    add celebrate validation

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const { celebrate, Joi } = require('celebrate');
 const mongoose = require('mongoose');
 const express = require('express');
+const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
 const {
   login,
@@ -14,7 +16,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signup', createUser);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required(),
+      password: Joi.string().required().min(8),
+    }).unknown(true),
+  }),
+  createUser,
+);
 app.post('/signin', login);
 
 app.use(auth);
@@ -26,14 +37,14 @@ app.use((req, res) => {
   res.status(404).send({ message: 'Запрошенной страницы не существует.' });
 });
 
+app.use(errors());
+
 app.use((err, req, res, next) => {
-  // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
 
   res
     .status(statusCode)
     .send({
-      // проверяем статус и выставляем сообщение в зависимости от него
       message: statusCode === 500
         ? 'На сервере произошла ошибка'
         : message,
