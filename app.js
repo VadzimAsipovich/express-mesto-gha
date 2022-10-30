4a846b7e1cec32688703f3d15d75f6af5cf65be4
vadzim.asipovich <vadimosipovichqa@yandex.by>
2022-10-31 00:11:25 +0300

    fffff

--- app.js
+++ app.js
@@ -33,7 +33,16 @@ app.post(
   }),
   createUser,
 );
-app.post('/signin', login);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().regex(emailPattern),
+      password: Joi.string().required().min(8),
+    }).unknown(true),
+  }),
+  login,
+);
 
 app.use(auth);
 
