39e4fe0d46c0adddacbbf6cff780e39e6edb6106
vadzim.asipovich <vadimosipovichqa@yandex.by>
2022-10-30 22:11:22 +0300

    sdfdsf

--- controllers/users.js
+++ controllers/users.js
@@ -122,7 +122,7 @@ module.exports.login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', {
         expiresIn: '7d',
       });
-      res.cookie('authorization', token, {
+      res.cookie('Authorization', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
       });
