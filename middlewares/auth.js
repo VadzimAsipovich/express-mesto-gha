4e301e6f5af31dfc810de39454e459aa8f8337b9
vadzim.asipovich <vadimosipovichqa@yandex.by>
2022-10-31 21:54:08 +0300

    auth.fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,5 @@
 const jwtApp = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-err');
-const BadRequestError = require('../errors/bad-request-err');
 
 module.exports = (req, res, next) => {
   const { jwt } = req.cookies;
@@ -13,9 +12,6 @@ module.exports = (req, res, next) => {
   try {
     payload = jwtApp.verify(jwt, 'some-secret-key');
   } catch (err) {
-    if (err.message === 'jwt malformed') {
-      throw new BadRequestError('Некорректный токен');
-    }
     throw new UnauthorizedError('Необходима авторизация');
   }
 
