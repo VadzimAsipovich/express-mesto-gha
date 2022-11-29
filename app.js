a93a9f857c63b538640d79be8be6f0b436da36d3
vadzim.asipovich <vadimosipovichqa@yandex.by>
2022-11-29 19:20:21 +0100

    add logging

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const NotFoundError = require('./errors/not-found-err');
 
@@ -14,6 +15,12 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestLogger);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use('/', require('./routes/index'));
 
@@ -25,7 +32,7 @@ app.use('/cards', require('./routes/cards'));
 app.use((req, res, next) => {
   next(new NotFoundError('Запрошенной страницы не существует.'));
 });
-
+app.use(errorLogger);
 app.use(errors());
 
 app.use(errorHandler);
