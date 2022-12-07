d8a9b85c33f44efe8c4a66ee4b14779ca6062876
vadzim.asipovich <vadimosipovichqa@yandex.by>
2022-12-07 21:58:25 +0100

    1

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Origin', 'http://vasipovich.backend.nomoredomains.club');
+  res.header('Access-Control-Allow-Origin', 'https://vasipovich.backend.nomoredomains.club');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
