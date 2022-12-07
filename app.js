46d71bfc4730fba3269a38c1446683eca2f0890d
vadzim.asipovich <vadimosipovichqa@yandex.by>
2022-12-07 22:39:48 +0100

    1

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
